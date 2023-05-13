5e9299deb7f596c914f0b49879fca23d3737794b
fatemeh_3104 <fatima.khoshgoftar@gmail.com>
2023-05-13 10:36:08 +0430

    add project in git Hub

--- database/migrations/2023_05_13_052347_create_posts_table.php
+++ database/migrations/2023_05_13_052347_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->softDeletes();
+            $table->softDeletes(); 
             $table->timestamps();
         });
     }
